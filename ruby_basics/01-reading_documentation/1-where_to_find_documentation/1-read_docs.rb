cd62068bce1475f8d69701050e4bc7fdb7e1f1d1
mike lederle <mrlederle@gmail.com>
2019-12-12 09:44:31 -0500

    [1.1] where to find documentation

---
+++ ruby_basics/01-reading_documentation/1-where_to_find_documentation/1-read_docs.rb
@@ -0,0 +1,22 @@
+# Where can you find the most complete Ruby documentation?
+#
+
+# (A) https://ruby-doc.org/ -- this is the Core Reference; says
+#                       -- pulled straight from source by RDoc
+#
+# Previously (A) was the official, now this is:
+# https://docs.ruby-lang.org/en/ -- Official API Documentation
+#
+# https://ruby-doc.org/stdlib-2.6.5/ -- Offical Std Library Docs
+#
+# https://docs.ruby-lang.org/en/master/extension_rdoc.html
+# Ruby C API Reference
+#
+# https://www.rubydoc.info/ -- Ruby gems info, github hosted projs
+#
+#
+# Core API: all classes and modules that are available without 
+# having to require them
+#
+# Standard Library: included in distribution but must be loaded
+#  in via require
