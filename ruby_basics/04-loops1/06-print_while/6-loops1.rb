3a65b19c3655d21555ea532ba6864cb3ff6596b6
mike lederle <mrlederle@gmail.com>
2019-12-13 18:42:04 -0500

    [4.6] print while

---
+++ ruby_basics/04-loops1/06-print_while/6-loops1.rb
@@ -0,0 +1,15 @@
+# Using a while loop, print 5 random numbers between 0 and 99. The code below shows an example of how to begin solving this exercise.
+
+numbers = []
+
+# while <condition>
+  # ...
+# end
+
+# Example output (your numbers will most likely be different):
+
+# 62
+# 96
+# 31
+# 16
+# 36
