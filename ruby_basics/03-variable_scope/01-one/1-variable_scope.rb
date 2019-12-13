75517936767f17e72db7a3b89610bf687b3213d0
mike lederle <mrlederle@gmail.com>
2019-12-13 11:11:23 -0500

    [3.1] integer assignment inside function

---
+++ ruby_basics/03-variable_scope/01-one/1-variable_scope.rb
@@ -0,0 +1,14 @@
+# What will the following code print and why? Don't run it until you have tried to answer.
+
+a = 7
+
+def my_value(b)
+  b += 10
+end
+
+my_value(a)
+puts a
+
+# my_value returns 17, but it is not saved anywhere so it disappears. The value of a remains the same: 7.
+
+
