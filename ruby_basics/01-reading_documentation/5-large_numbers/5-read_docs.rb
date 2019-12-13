13cc2cf23818aa3c5d76ddadbff2620286454204
mike lederle <mrlederle@gmail.com>
2019-12-13 09:19:45 -0500

    [1.5] integer literal formats

---
+++ ruby_basics/01-reading_documentation/5-large_numbers/5-read_docs.rb
@@ -0,0 +1,10 @@
+# Using the ruby documentation, determine how you can write large numbers in a way that makes them easier to read.
+
+# Numbers
+
+# You can write integers of any size as follows:
+
+p 1234
+p 1_234
+ 
+# These numbers have the same value, 1,234. The underscore may be used to enhance readability for humans. You may place an underscore anywhere in the number.
