188be0424428c0709265340fa9b61b4cff171f23
mike lederle <mrlederle@gmail.com>
2019-12-13 12:39:53 -0500

    [3.10] block wrapped in method

---
+++ ruby_basics/03-variable_scope/10-ten/10-variable_scope.rb
@@ -0,0 +1,15 @@
+# What will the following code print, and why? Don't run the code until you have tried to answer.
+
+a = 7
+array = [1, 2, 3]
+
+def my_value(ary)
+  ary.each do |b|
+    a += b
+  end
+end
+
+my_value(array)
+puts a
+
+# the outer a is not visible on line a, so the operation fails.
