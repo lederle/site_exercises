2784f37fde64eb696dc415ed95e472cd93578956
mike lederle <mrlederle@gmail.com>
2019-12-13 19:06:12 -0500

    [4.6] print 5 rand ints

--- ruby_basics/04-loops1/06-print_while/6-loops1.rb
+++ ruby_basics/04-loops1/06-print_while/6-loops1.rb
@@ -1,7 +1,11 @@
 # Using a while loop, print 5 random numbers between 0 and 99. The code below shows an example of how to begin solving this exercise.
 
-numbers = []
-
+numbers = [rand(100), rand(100), rand(100), rand(100), rand(100)]
+count = 0
+while count != 5
+  puts numbers[count]
+  count += 1
+end
 # while <condition>
   # ...
 # end
