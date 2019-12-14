f927b571869016152196d1429bdd800343efc141
mike lederle <mrlederle@gmail.com>
2019-12-14 16:50:38 -0500

    [6.6] enter and check password

--- ruby_basics/06-user_input/06-passwords/6-user_input.rb
+++ ruby_basics/06-user_input/06-passwords/6-user_input.rb
@@ -13,3 +13,12 @@
 # SecreT
 # Welcome!
 # 
+
+PASSWORD = "SecreT"
+loop do
+  puts "Please enter your password:"
+  password = gets.chomp
+  break if password == PASSWORD
+  puts "Invalid password!"
+end
+puts "Welcome!"
