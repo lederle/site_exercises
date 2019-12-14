2384fd8dce5c3f4f0695f08f8b0b82b404a60170
mike lederle <mrlederle@gmail.com>
2019-12-14 09:50:25 -0500

    [5.6] empty the array

---
+++ ruby_basics/05-loops2/06-empty_the_array/6-loops2.rb
@@ -0,0 +1,6 @@
+# Given the array below, use loop to remove and print each name. Stop the loop once names doesn't contain any more elements.
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+# Keep in mind to only use loop, not while, until, etc.
+
