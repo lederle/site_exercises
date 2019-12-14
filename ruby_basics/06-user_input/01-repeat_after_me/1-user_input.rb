528eaf28d14702360904c02abca8d58603fb2a6d
mike lederle <mrlederle@gmail.com>
2019-12-14 14:46:38 -0500

    [6.1] repeat after me

---
+++ ruby_basics/06-user_input/01-repeat_after_me/1-user_input.rb
@@ -0,0 +1,9 @@
+# Write a program that asks the user to type something in, after which your program should simply display what was entered.
+
+# Example:
+
+# $ ruby repeater.rb
+# >> Type anything you want:
+# This is what I typed.
+# This is what I typed.
+
