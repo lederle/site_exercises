4fd5f9f7d6a7c3d56cd8030be6586e15d0adf704
mike lederle <mrlederle@gmail.com>
2019-12-14 14:50:41 -0500

    [6.2] your age in months

---
+++ ruby_basics/06-user_input/02-your_age_in_months/2-user_input.rb
@@ -0,0 +1,9 @@
+# Write a program that asks the user for their age in years, and then converts that age to months.
+
+# Examples:
+
+# $ ruby age.rb
+# >> What is your age in years?
+# 35
+# You are 420 months old.
+
