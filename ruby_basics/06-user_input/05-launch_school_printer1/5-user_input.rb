15612e134cb11ff5dfdae78c1976e2deece27bf8
mike lederle <mrlederle@gmail.com>
2019-12-14 16:20:50 -0500

    [6.5] print launch school n times

---
+++ ruby_basics/06-user_input/05-launch_school_printer1/5-user_input.rb
@@ -0,0 +1,36 @@
+# Write a program that prints 'Launch School is the best!' repeatedly until a certain number of lines have been printed. The program should obtain the number of lines from the user, and should insist that at least 3 lines are printed.
+# 
+# For now, just use #to_i to convert the input value to an Integer, and check that result instead of trying to insist on a valid number; validation of numeric input is a topic with a fair number of edge conditions that are beyond the scope of this exercise.
+# 
+# Examples:
+# 
+# $ ruby lsprint.rb
+# >> How many output lines do you want? Enter a number >= 3:
+# 5
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+# 
+# $ ruby lsprint.rb
+# >> How many output lines do you want? Enter a number >= 3:
+# 2
+# >> That's not enough lines.
+# >> How many output lines do you want? Enter a number >= 3:
+# 3
+# Launch School is the best!
+# Launch School is the best!
+# Launch School is the best!
+
+count = 0
+loop do
+  puts "How many output lines do you want? Enter a number >= 3:"
+  count = gets.chomp.to_i
+  break if count > 2
+  puts "That's not enough lines."
+end
+
+count.times do
+  puts "Launch School is the best!"
+end
