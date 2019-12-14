8d8af6ef834d99cf5427f65900eee841532a2ccd
mike lederle <mrlederle@gmail.com>
2019-12-14 16:40:20 -0500

    [6.6] passwords

---
+++ ruby_basics/06-user_input/06-passwords/6-user_input.rb
@@ -0,0 +1,15 @@
+# Write a program that displays a welcome message, but only after the user enters the correct password, where the password is a string that is defined as a constant in your program. Keep asking for the password until the user enters the correct password.
+# 
+# Examples:
+# 
+# $ ruby password.rb
+# >> Please enter your password:
+# Hello
+# >> Invalid password!
+# >> Please enter your password:
+# Secret
+# >> Invalid password!
+# >> Please enter your password:
+# SecreT
+# Welcome!
+# 
