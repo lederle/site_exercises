431ea81bdd388c16f3abafaa06257d7db00bcacc
mike lederle <mrlederle@gmail.com>
2019-12-15 07:01:35 -0500

    [6.9] launch school printer part 2

--- ruby_basics/06-user_input/09-launch_school_printer2/9-user_input.rb
+++ ruby_basics/06-user_input/09-launch_school_printer2/9-user_input.rb
@@ -1,19 +1,31 @@
 # In an earlier exercise, you wrote a program that prints 'Launch School is the best!' repeatedly until a certain number of lines have been printed. Our solution looked like this:
 
-number_of_lines = nil
-loop do
-  puts '>> How many output lines do you want? Enter a number >= 3:'
-  number_of_lines = gets.to_i
-  break if number_of_lines >= 3
-  puts ">> That's not enough lines."
-end
-
-while number_of_lines > 0
-  puts 'Launch School is the best!'
-  number_of_lines -= 1
-end
+# number_of_lines = nil
+# loop do
+#   puts '>> How many output lines do you want? Enter a number >= 3:'
+#   number_of_lines = gets.to_i
+#   break if number_of_lines >= 3
+#   puts ">> That's not enough lines."
+# end
+# 
+# while number_of_lines > 0
+#   puts 'Launch School is the best!'
+#   number_of_lines -= 1
+# end
 
 # Modify this program so it repeats itself after each input/print iteration, asking for a new number each time through. The program should keep running until the user enters q or Q.
+
+loop do
+  puts "How many output lines do you want? Enter a number >= 3 (Q to quit):"
+  input = gets.chomp.downcase
+  break if input == 'q'
+  count = input.to_i
+  next puts "That's not enough lines." if count < 3
+  until count == 0
+    puts "Launch School is the best!"
+    count -= 1
+  end
+end
 
 # Examples:
 
