d160299991f15cd4b5feab3f7755fa2fd0b72480
mike lederle <mrlederle@gmail.com>
2019-12-15 08:57:47 -0500

    [8.9] counting sheep part 3

---
+++ ruby_basics/08-return/09-counting_sheep3/9-return.rb
@@ -0,0 +1,12 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+    if sheep >= 2
+      return "aergf"
+    end
+  end
+end
+
+p count_sheep
