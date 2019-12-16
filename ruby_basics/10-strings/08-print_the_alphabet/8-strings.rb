1f9afc67ae278734bb23f7b3453863d0f7b0f4ca
mike lederle <mrlederle@gmail.com>
2019-12-16 11:11:28 -0500

    [10.8] print the alphabet

---
+++ ruby_basics/10-strings/08-print_the_alphabet/8-strings.rb
@@ -0,0 +1,34 @@
+# Using the following code, split the value of alphabet by individual characters and print each character.
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+alphabet.split('').each {|c| puts c}
+
+# Expected output:
+
+# a
+# b
+# c
+# d
+# e
+# f
+# g
+# h
+# i
+# j
+# k
+# l
+# m
+# n
+# o
+# p
+# q
+# r
+# s
+# t
+# u
+# v
+# w
+# x
+# y
+# z
