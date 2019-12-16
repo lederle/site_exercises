fdf7d1b225501809e00b381f083caaecfbe7aa7c
mike lederle <mrlederle@gmail.com>
2019-12-16 13:10:37 -0500

    [12.10] which collection

---
+++ ruby_basics/12-hashes/10-which_collection/10-hashes.rb
@@ -0,0 +1,12 @@
+# Rewrite car as a nested array containing the same key-value pairs.
+car = [[:type, 'sedan'], [:color, 'blue'], [:year, 2003]]
+
+# nested array, not hash
+# h = {
+#   car: {
+#     type:  'sedan',
+#     color: 'blue',
+#     year:  2003
+#   }
+# }
+
