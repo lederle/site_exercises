2b54a3f7d9da22dab16f8c1dfff5881a81edb34b
mike lederle <mrlederle@gmail.com>
2019-12-16 11:35:00 -0500

    [11.1] new pet

---
+++ ruby_basics/11-arrays/01-new_pet/1-arrays.rb
@@ -0,0 +1,12 @@
+# In the code below, an array containing different types of pets is assigned to pets.
+
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+# Select 'fish' from pets, assign the return value to a variable named my_pet, then print the value of my_pet.
+
+# Expected output:
+
+# I have a pet fish!
+
+my_pets = pets.select { |pet| pet == 'fish' }
+puts my_pets
