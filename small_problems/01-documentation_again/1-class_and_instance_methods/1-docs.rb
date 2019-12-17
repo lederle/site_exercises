15e94cb464671dd519feba19ec39b313b4e4740c
mike lederle <mrlederle@gmail.com>
2019-12-17 16:58:38 -0500

    [1.1sp] class vs instance methods

---
+++ small_problems/01-documentation_again/1-class_and_instance_methods/1-docs.rb
@@ -0,0 +1,6 @@
+# Locate the ruby documentation for methods File::path and File#path. How are they different?
+
+# One is a class method and the other is an instance method
+#
+# File::path -- Returns the string representation of the path
+# File#path  -- Returns the pathname used to create file as a string. Does not normalize the name.
